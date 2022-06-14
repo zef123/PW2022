561862dc8e1a0927c569c36a0b84b30bbb9edb2e
zef123 <92774524+zef123@users.noreply.github.com>
2022-06-15 00:04:12 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -7,7 +7,7 @@ error_reporting(0);
 session_start();
  
 if (isset($_SESSION['username'])) {
-    header("Location: berhasil_login.php");
+    header("Location: index.html");
 }
  
 if (isset($_POST['submit'])) {
@@ -19,7 +19,7 @@ if (isset($_POST['submit'])) {
     if ($result->num_rows > 0) {
         $row = mysqli_fetch_assoc($result);
         $_SESSION['username'] = $row['username'];
-        header("Location: berhasil_login.php");
+        header("Location: index.html");
     } else {
         echo "<script>alert('Email atau password Anda salah. Silahkan coba lagi!')</script>";
     }
